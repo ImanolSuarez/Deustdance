55b5eba28367f0d978b243b5189f5700eb41fc66
Xabier Hernaez <xabier.h.s@opendeusto.es>
2023-10-13 20:02:16 +0200

    Clase madre.

---
+++ deustDance/src/deustDance/Persona.java
@@ -0,0 +1,146 @@
+package deustDance;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Persona implements Comparable<Persona>{
+	
+	/*COMPONENTES DE LA CLASE PERSONA*/
+	
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	private String dni;
+	private Date fNac;
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+	private String direccion;
+	private String codPostal;
+	private String telefono;
+	
+	/*CONSTRUCTOR DE LA CLASE*/
+	
+	public Persona(String nombre, String apellidos, int edad, String dni, Date fNac, String direccion,
+			String codPostal, String telefono) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+		this.dni = dni;
+		this.fNac = fNac;
+		this.direccion = direccion;
+		this.codPostal = codPostal;
+		this.telefono = telefono;
+	}
+	
+	public Persona(String nombre, String apellidos, int edad, String dni, String fNac, String direccion,
+			String codPostal, String telefono) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+		this.dni = dni;
+		try {
+			this.fNac = sdf.parse(fNac);
+		} catch (ParseException e) {
+			System.out.println("Fecha erronea");
+		}
+		this.direccion = direccion;
+		this.codPostal = codPostal;
+		this.telefono = telefono;
+	}
+	
+	/*GETTERS Y SETTERS DE LA CLASE*/
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public Date getfNac() {
+		return fNac;
+	}
+
+	public void setfNac(Date fNac) {
+		this.fNac = fNac;
+	}
+	
+	public String StrFNac() {
+		return sdf.format(fNac);
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCodPostal() {
+		return codPostal;
+	}
+
+	public void setCodPostal(String codPostal) {
+		this.codPostal = codPostal;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	@Override
+	public String toString() {
+		return "Nombre: " + nombre + ", apellidos: " + apellidos + ", edad: " + edad + ", dni: " + dni + ", fecha de nacimiento: "
+				+ sdf.format(fNac) + ", direccion: " + direccion + ", codigo postal: " + codPostal + ", telefono: " + telefono;
+	}
+
+	@Override
+	public int compareTo(Persona o) {
+		return dni.compareTo(o.dni);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
