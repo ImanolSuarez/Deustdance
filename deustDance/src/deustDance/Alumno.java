34bbf5af881f4d85aeb3f2898c8f175cc08de21b
Xabier Hernaez <xabier.h.s@opendeusto.es>
2023-10-13 20:47:10 +0200

    Correccion de la clase Alumno

--- deustDance/src/deustDance/Alumno.java
+++ deustDance/src/deustDance/Alumno.java
@@ -6,11 +6,13 @@ public class Alumno extends Persona{
 	
 	private Baile baileAsignado;
 	private Profesor profesorAsignado;
+	private Clase claseAsignada;
 	private double dinero;
 	
 	public Alumno(String nombre, String apellidos, int edad, String dni, Date fNac, String direccion, String codPostal,
-			String telefono, Baile baileAsignado, Profesor profesorAsignado, double dinero) {
+			String telefono, Baile baileAsignado, Profesor profesorAsignado, double dinero, Clase claseAsignada) {
 		super(nombre, apellidos, edad, dni, fNac, direccion, codPostal, telefono);
+		this.claseAsignada = claseAsignada;
 		this.baileAsignado = baileAsignado;
 		this.profesorAsignado = profesorAsignado;
 		this.dinero = dinero;
@@ -45,12 +47,20 @@ public class Alumno extends Persona{
 	public void setDinero(double dinero) {
 		this.dinero = dinero;
 	}
+	
+	public Clase getClaseAsignada() {
+		return claseAsignada;
+	}
+
+	public void setClaseAsignada(Clase claseAsignada) {
+		this.claseAsignada = claseAsignada;
+	}
 
 	@Override
 	public String toString() {
 		return "Nombre: " + getNombre() + ", Apellidos: " + getApellidos() + ", Edad: " + getEdad()
 				+ ", Dni: " + getDni() + ", Fecha de nacimiento" + getfNac() + ", baile asignado=" + baileAsignado
-				+ ", profesor asignado=" + profesorAsignado;
+				+ ", profesor asignado=" + profesorAsignado + ", clase asignada: " + claseAsignada;
 	}
 
 	
