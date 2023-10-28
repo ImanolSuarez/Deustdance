0b6cd46a155c49b1269de6044cc46013bc5a0c59
paula <paula@192.168.1.149>
2023-10-28 20:28:51 +0200

    Creación de pestañas; JTextFields y Botones de la pestaña Alumno.

--- deustDance/src/ventanas/VentanaSecretaria.java
+++ deustDance/src/ventanas/VentanaSecretaria.java
@@ -1,5 +1,7 @@
 package ventanas;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,29 +12,75 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
 public class VentanaSecretaria extends JFrame {
 	protected JTabbedPane pestañas;
+	protected JTextField txtNombre;
+	protected JSpinner spinnerGrupo;
+	protected JTextField txtDomicilio;
+	protected JTextField txtUsuario;
+	protected JTextField txtContrasenya;
+	protected JTextField txtTelefono;
+	protected JButton botonAnyadir;
+	protected JButton botonModificar;
+	protected JButton botonEliminar;
 	
 	public VentanaSecretaria() {
 		
 		JTabbedPane tabbedPane = new JTabbedPane();
 		
 		// Pestaña Alumnos
-        JPanel panel1 = new JPanel();
-        panel1.add(new JLabel("Contenido de la Pestaña 1"));
-        tabbedPane.addTab("Alumnos", panel1);
+		txtNombre = new JTextField();
+		spinnerGrupo = new JSpinner();
+		txtDomicilio = new JTextField();
+		txtUsuario = new JTextField();
+		txtContrasenya = new JTextField();
+		txtTelefono = new JTextField();
+		botonAnyadir = new JButton("Añadir alumno");
+		botonModificar = new JButton("Modificar alumno");
+		botonEliminar = new JButton("Eliminar alumno");
+		
+		JPanel panelTexto = new JPanel();
+		panelTexto.setLayout(new GridLayout(6,2));
+        
+		panelTexto.add(new JLabel("Nombre y Apellidos: "));
+		panelTexto.add(txtNombre);
+		panelTexto.add(new JLabel("Grupo: "));
+		panelTexto.add(spinnerGrupo);
+		panelTexto.add(new JLabel("Domicilio: "));
+		panelTexto.add(txtDomicilio);
+		panelTexto.add(new JLabel("Usuario: "));
+		panelTexto.add(txtUsuario);
+		panelTexto.add(new JLabel("Contraseña: "));
+		panelTexto.add(txtContrasenya);
+		
+		JPanel panelBotones = new JPanel();
+		panelBotones.setLayout(new GridLayout(1,3));
+		panelBotones.add(botonAnyadir);
+		panelBotones.add(botonModificar);
+		panelBotones.add(botonEliminar);
+		
+		JPanel panelDatos = new JPanel();
+		panelDatos.setLayout(new GridLayout(2,1));
+		panelDatos.add(panelTexto);
+		panelDatos.add(panelBotones);
+		
+		JPanel panelAlumnos = new JPanel();
+        panelAlumnos.add(panelDatos, BorderLayout.EAST);
+        tabbedPane.addTab("Alumnos", panelAlumnos);
 
         // Pestaña Profesores
-        JPanel panel2 = new JPanel();
-        panel2.add(new JLabel("Contenido de la Pestaña 2"));
-        tabbedPane.addTab("Profesores", panel2);
+        JPanel panelProfesores = new JPanel();
+        panelProfesores.add(new JLabel("Contenido de la Pestaña 2"));
+        tabbedPane.addTab("Profesores", panelProfesores);
 
         // Pestaña Horario
-        JPanel panel3 = new JPanel();
-        panel3.add(new JLabel("Contenido de la Pestaña 3"));
-        tabbedPane.addTab("Horario", panel3);
+        JPanel panelHorario = new JPanel();
+        panelHorario.add(new JLabel("Contenido de la Pestaña 3"));
+        tabbedPane.addTab("Horario", panelHorario);
 
         this.add(tabbedPane);	
 		
