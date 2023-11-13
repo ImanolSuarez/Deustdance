66b369cfc0510bd2cbab0231e7572be1407c0137
XabierHernaez <xabier.h.s@opendeusto.es>
2023-11-13 21:40:30 +0100

    ClaseTest creado, 100%

---
+++ deustDance/test/deustDance/ClaseTest.java
@@ -0,0 +1,49 @@
+package deustDance;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClaseTest {
+	
+	private Clase c;
+	
+	@Before
+	public void setUp() {
+		c = new Clase("Clase", null, null);
+	}
+	@Test
+	public void getNombre() {
+		assertEquals("Clase", c.getNombre());
+	}
+	@Test
+	public void setNombre() {
+		c.setNombre("Clase");
+		assertEquals("Clase", c.getNombre());
+	}
+	@Test
+	public void getListaProfesores() {
+		assertNull(c.getListaProfesores());
+	}
+	@Test
+	public void setListaProfesores() {
+		c.setListaProfesores(null);
+		assertNull(c.getListaProfesores());
+	}
+	@Test
+	public void getListaAlumnos() {
+		assertNull(c.getListaAlumnos());
+	}
+	@Test
+	public void setListaAlumnos() {
+		c.setListaAlumnos(null);
+		assertNull(c.getListaAlumnos());
+	}
+	@Test
+	public void ClaseToString() {
+		assertEquals("Nombre de la clase: " + "Clase" + ", lista de profesores: " + null 
+					+ ", lista de alumnos=" + null, c.toString());
+	}
+}
