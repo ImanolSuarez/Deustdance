1cbd26a78684b77183c27703825ac5a33c89e94a
XabierHernaez <xabier.h.s@opendeusto.es>
2023-11-13 21:15:52 +0100

    SecretariaTest creado, 100%

---
+++ deustDance/test/deustDance/SecretariaTest.java
@@ -0,0 +1,27 @@
+package deustDance;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SecretariaTest {
+	
+	private Secretaria s;
+	
+	
+	@Before
+	public void setUp() {
+		s = new Secretaria("Secretaria", "Apellido", "usuario", "contraseña");
+	}
+	
+	
+	@Test
+	public void SecretariaToString() {
+		assertEquals("Nombre de la secretaria: " + "Secretaria" + ", apellidos: " + "Apellido" + ", usuario: "
+				+ "usuario" + ", contraseña " + "contraseña", s.toString());
+	}
+
+}
