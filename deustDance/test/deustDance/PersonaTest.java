628303198f0f7248a6ba2962f0f4b12ac2f76ea3
XabierHernaez <xabier.h.s@opendeusto.es>
2023-11-26 12:22:09 +0100

    PersonaTest error solucionado.

--- deustDance/test/deustDance/PersonaTest.java
+++ deustDance/test/deustDance/PersonaTest.java
@@ -2,71 +2,84 @@ package deustDance;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PersonaTest {
 	
+	
+	private Persona p;
+	@Before
+	public void setUp() {
+		p = new Persona("Nombre1", "Apellidos1", "Usuario", "Contraseña", 67787765, "domicilio");
+	}
+	
 	@Test
 	public void testGetNombre() {
-		Persona p1 = new Persona("Nombre1", "Apellidos1", "Usuario-Nombre1", "Contraseña-Nombre1");
-		assertEquals("Nombre1", p1.getNombre());
+		assertEquals("Nombre1", p.getNombre());
 	}
 	
 	@Test
 	public void testSetNombre() {
-		Persona p1 = new Persona("Nombre1", "Apellidos1", "Usuario-Nombre1", "Contraseña-Nombre1");
-		p1.setNombre("Nombre1");
-		assertEquals("Nombre1", p1.getNombre());
+		p.setNombre("Nombre1");
+		assertEquals("Nombre1", p.getNombre());
 	}
-	
 	@Test
 	public void testGetApellidos() {
-		Persona p2 = new Persona("Nombre2", "Apellidos2", "Usuario-Nombre2", "Contraseña-Nombre2");
-		assertEquals("Apellidos2", p2.getApellidos());
-		
+		assertEquals("Apellidos1", p.getApellidos());
 	}
 	
 	@Test
 	public void testSetApellidos() {
-		Persona p2 = new Persona("Nombre2", "Apellidos2", "Usuario-Nombre2", "Contraseña-Nombre2");
-		p2.setApellidos("Apellidos2");
-		assertEquals("Apellidos2", p2.getApellidos());
+		p.setApellidos("Apellidos1");
+		assertEquals("Apellidos1", p.getApellidos());
 		
 	}
 	
 	@Test
 	public void testGetUsuario() {
-		Persona p3 = new Persona("Nombre3", "Apellidos3", "Usuario-Nombre3", "Contraseña-Nombre3");
-		assertEquals("Usuario-Nombre3", p3.getUsuario());
+		assertEquals("Usuario", p.getUsuario());
 		
 	}
 	
 	@Test
 	public void testSetUsuario() {
-		Persona p3 = new Persona("Nombre3", "Apellidos3", "Usuario-Nombre3", "Contraseña-Nombre3");
-		p3.setUsuario("Usuario-Nombre3");
-		assertEquals("Usuario-Nombre3", p3.getUsuario());
+		p.setUsuario("Usuario");
+		assertEquals("Usuario", p.getUsuario());
 		
 	}
 	
 	@Test
 	public void testGetContrasenia() {
-		Persona p4 = new Persona("Nombre4", "Apellidos4", "Usuario-Nombre4", "Contraseña-Nombre4");
-		assertEquals("Contraseña-Nombre4", p4.getContrasenia());
+		assertEquals("Contraseña", p.getContrasenia());
 	}
-	
 	@Test
 	public void testSetContrasenia() {
-		Persona p4 = new Persona("Nombre4", "Apellidos4", "Usuario-Nombre4", "Contraseña-Nombre4");
-		p4.setContrasenia("Contraseña-Nombre4");
-		assertEquals("Contraseña-Nombre4", p4.getContrasenia());
+		p.setContrasenia("Contraseña");
+		assertEquals("Contraseña", p.getContrasenia());
 	}
-	
+	@Test
+	public void testgetTelefono() {
+		assertEquals(67787765, p.getTelefono());
+	}
+	@Test
+	public void testsetTelefono() {
+		p.setTelefono(67787765);
+		assertEquals(67787765, p.getTelefono());
+	}
+	@Test
+	public void getDomicilio() {
+		assertEquals("domicilio", p.getDomicilio());
+	}
+	@Test
+	public void setDomicilio() {
+		p.setDomicilio("domicilio");
+		assertEquals("domicilio", p.getDomicilio());
+	}
 	@Test
 	public void PersonaToString() {
-		Persona p4 = new Persona("Nombre4", "Apellidos4", "Usuario-Nombre4", "Contraseña-Nombre4");
-		assertEquals("nombre: " + "Nombre4" + ", apellidos: " + "Apellidos4"  + ", usuario: " + "Usuario-Nombre4"
-				+ ", contrasenia: " + "Contraseña-Nombre4", p4.toString());
+		assertEquals("Persona [nombre=" + "Nombre1" + ", apellidos=" + "Apellidos1"  + ", usuario=" + "Usuario"
+				+ ", contrasenia=" + "Contraseña" + ", telefono=" + 67787765 + ", domicilio=" + "domicilio" + "]", p.toString());
 	}
 
 }
